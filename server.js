5ed4c2991596eebeac729ca90229c9ef7f9bb37d
yoonjh5032 <yoonjh5032@gmail.com>
2026-03-31 04:55:14 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const User = require('./models/User');
 const app  = express();
 const PORT = process.env.PORT || 4000;
 
+app.set('trust proxy', 1);
+
 /* ── 미들웨어 ── */
 app.use(helmet());
 app.use(express.json({ limit: '2mb' }));
